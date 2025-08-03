de945d39e036ae382bb1bcebc0725cc0a15bce90
Alexssander Fonseca <alex.marques.fa@gmail.com>
2025-08-03 11:45:25 -0300

    feat: demais features

--- src/main/java/br/com/alexsdm/postech/oficina/ordemServico/model/OrdemServico.java
+++ src/main/java/br/com/alexsdm/postech/oficina/ordemServico/model/OrdemServico.java
@@ -1,44 +1,111 @@
 package br.com.alexsdm.postech.oficina.ordemServico.model;
 
-import br.com.alexsdm.postech.oficina.clientes.model.Cliente;
-import br.com.alexsdm.postech.oficina.pecas.model.Peca;
-import br.com.alexsdm.postech.oficina.servicos.model.Servico;
+import br.com.alexsdm.postech.oficina.cliente.model.Cliente;
+import br.com.alexsdm.postech.oficina.servico.model.Servico;
+import jakarta.persistence.*;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class OrdemServico {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
     private Cliente cliente;
-    private List<Peca> pecas;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "ordem_servico_id")
+    private List<ItemPecaOrdemServico> itensPecaOrdemServico;
+    @ManyToMany
+    @JoinTable(name = "ordem_servico_servico",
+            joinColumns = @JoinColumn(name = "ordem_servico_id"),
+            inverseJoinColumns = @JoinColumn(name = "servico_id"))
     private List<Servico> servicos;
     private Status status;
+    private LocalDateTime dataCriacao;
+    private Instant instanteInicioDaExecucao;
+    private Instant instanteFimDaExecucao;
+    private Instant instanteEntrega;
 
-    public OrdemServico(Cliente cliente,
-                        List<Peca> pecas,
-                        List<Servico> servicos,
-                        Status status) {
-        this.cliente = cliente;
-        this.pecas = pecas;
-        this.servicos = servicos;
-        this.status = status;
+    public OrdemServico() {
     }
 
-    public void executar() {
+    public OrdemServico(Cliente cliente,
+                        Status status) {
+        this.cliente = cliente;
+        this.itensPecaOrdemServico = new ArrayList<>();
+        this.servicos = new ArrayList<>();
+        this.status = status;
+        this.dataCriacao = LocalDateTime.now();
+    }
+
+    public void executar(List<ItemPecaOrdemServico> itens, List<Servico> servicos) {
         this.status = Status.EM_EXECUCAO;
+        this.itensPecaOrdemServico.clear();            // Remove os antigos (serão deletados pelo orphanRemoval)
+        this.itensPecaOrdemServico.addAll(itens);      // Adiciona os novos
+
+        this.servicos.clear();                         // Mesmo raciocínio, se quiser limpar
+        this.servicos.addAll(servicos);                // Adiciona novos serviços
+
+        this.instanteInicioDaExecucao = Instant.now();
     }
 
     public void diagnosticar() {
         this.status = Status.EM_DIAGNOSTICO;
     }
 
-    public void adicionarPeca(Peca peca) {
-        this.pecas.add(peca);
+    public void finalizarDiagnostico() {
+        this.status = Status.AGUARDANDO_APROVACAO;
     }
 
-    public void adicionarServico(Servico servico) {
-        this.servicos.add(servico);
+    public void finalizar() {
+        this.status = Status.FINALIZADA;
+        this.instanteFimDaExecucao = Instant.now();
+    }
+
+    public void entregar() {
+        this.status = Status.ENTREGUE;
+        this.instanteEntrega = Instant.now();
     }
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public List<ItemPecaOrdemServico> getItensPecaOrdemServico() {
+        return itensPecaOrdemServico;
+    }
+
+    public List<Servico> getServicos() {
+        return servicos;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public Instant getInstanteInicioDaExecucao() {
+        return instanteInicioDaExecucao;
+    }
+
+    public Instant getInstanteFimDaExecucao() {
+        return instanteFimDaExecucao;
+    }
+
+    public Instant getInstanteEntrega() {
+        return instanteEntrega;
+    }
 }
--- src/main/java/br/com/alexsdm/postech/oficina/ordemServico/service/OrdemServicoService.java
+++ src/main/java/br/com/alexsdm/postech/oficina/ordemServico/service/OrdemServicoService.java
@@ -1,47 +1,134 @@
 package br.com.alexsdm.postech.oficina.ordemServico.service;
 
-import br.com.alexsdm.postech.oficina.clientes.repository.ClienteRepository;
-import br.com.alexsdm.postech.oficina.orcamento.repository.OrcamentoRepository;
+import br.com.alexsdm.postech.oficina.cliente.repository.ClienteRepository;
+import br.com.alexsdm.postech.oficina.orcamento.service.OrcamentoService;
+import br.com.alexsdm.postech.oficina.orcamento.service.input.PecaOrcamentoInput;
 import br.com.alexsdm.postech.oficina.ordemServico.controller.request.CriarOrdemDeServicoRequest;
+import br.com.alexsdm.postech.oficina.ordemServico.model.ItemPecaOrdemServico;
 import br.com.alexsdm.postech.oficina.ordemServico.model.OrdemServico;
 import br.com.alexsdm.postech.oficina.ordemServico.model.Status;
-import br.com.alexsdm.postech.oficina.pecas.repository.PecaRepository;
-import br.com.alexsdm.postech.oficina.servicos.repository.ServicoRepository;
-import br.com.alexsdm.postech.oficina.veiculos.repository.VeiculoModeloRepository;
+import br.com.alexsdm.postech.oficina.ordemServico.repository.OrdemServicoRepository;
+import br.com.alexsdm.postech.oficina.ordemServico.service.input.OsPecaNecessariasInput;
+import br.com.alexsdm.postech.oficina.peca.repository.PecaRepository;
+import br.com.alexsdm.postech.oficina.servico.repository.ServicoRepository;
+import br.com.alexsdm.postech.oficina.veiculo.repository.VeiculoModeloRepository;
+import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.List;
 
+@Service
 public class OrdemServicoService {
 
     private final ClienteRepository clienteRepository;
     private final VeiculoModeloRepository veiculoModeloRepository;
-    private final OrcamentoRepository orcamentoRepository;
     private final PecaRepository pecasRepository;
     private final ServicoRepository servicoRepository;
+    private final OrdemServicoRepository ordemServicoRepository;
+    private final OrcamentoService orcamentoService;
 
     public OrdemServicoService(ClienteRepository clienteRepository,
                                VeiculoModeloRepository veiculoModeloRepository,
-                               OrcamentoRepository orcamentoRepository,
                                PecaRepository pecasRepository,
-                               ServicoRepository servicoRepository) {
+                               ServicoRepository servicoRepository, OrdemServicoRepository ordemServicoRepository, OrcamentoService orcamentoService) {
         this.clienteRepository = clienteRepository;
         this.veiculoModeloRepository = veiculoModeloRepository;
-        this.orcamentoRepository = orcamentoRepository;
         this.pecasRepository = pecasRepository;
         this.servicoRepository = servicoRepository;
+        this.ordemServicoRepository = ordemServicoRepository;
+        this.orcamentoService = orcamentoService;
     }
 
-    public void criar(CriarOrdemDeServicoRequest criarOrdemDeServicoRequest) {
+    public OrdemServico criar(CriarOrdemDeServicoRequest criarOrdemDeServicoRequest) {
         var cliente = clienteRepository.findByCpfCnpj(criarOrdemDeServicoRequest.cpfCnpj())
                 .orElseThrow(RuntimeException::new);
 
-        var ordemService = new OrdemServico(
+        var orcamentoId = criarOrdemDeServicoRequest.orcamentoId();
+
+        if (orcamentoId != null) {
+            var orcamento = orcamentoService.buscarPorId(orcamentoId)
+                    .orElseThrow(RuntimeException::new);
+            if (!orcamento.isAceito()) {
+                throw new RuntimeException("dsaodsaj");
+            }
+
+            var ordemServico = new OrdemServico(
+                    cliente,
+                    Status.RECEBIDA
+            );
+
+            ordemServicoRepository.save(ordemServico);
+            return ordemServico;
+
+        }
+        var ordemServico = new OrdemServico(
                 cliente,
-                Collections.emptyList(),
-                Collections.emptyList(),
                 Status.RECEBIDA
         );
+        ordemServicoRepository.save(ordemServico);
+        return ordemServico;
+    }
 
+    public OrdemServico iniciarDiagnostico(Long idOrdemServico) {
+        var ordemServico = buscarOrdemServico(idOrdemServico);
+        ordemServico.diagnosticar();
+        ordemServicoRepository.save(ordemServico);
+        return ordemServico;
+    }
 
+    public OrdemServico finalizarDiagnostico(Long idOrdemServico,
+                                             List<OsPecaNecessariasInput> osPecaNecessariasInputs,
+                                             List<Long> idServicosNecessarios) {
+        var ordemServico = buscarOrdemServico(idOrdemServico);
+        ordemServico.finalizarDiagnostico();
+        ordemServicoRepository.save(ordemServico);
+        var orcamentoInput = osPecaNecessariasInputs.stream()
+                .map(osPecaNecessariasInput ->
+                        new PecaOrcamentoInput(osPecaNecessariasInput.pecaId(), osPecaNecessariasInput.qtd()))
+                .toList();
+
+        orcamentoService.criar(
+                ordemServico.getCliente().getCpfCnpj(),
+                orcamentoInput,
+                idServicosNecessarios);
+
+        return ordemServico;
+
+    }
+
+    public OrdemServico executar(Long idOrdemServico, Long orcamentoID) {
+        var ordemServico = buscarOrdemServico(idOrdemServico);
+        var orcamento = orcamentoService.buscarPorId(orcamentoID).orElseThrow(RuntimeException::new);
+        //        if (!orcamento.isAceito()) {
+        //            throw new RuntimeException("dsaodsaj");
+        //        }
+        var itemPecaOS = orcamento.getItensPeca().stream()
+                .map(itemPecaOrcamento -> {
+                    itemPecaOrcamento.getPeca().retirar(itemPecaOrcamento.getQuantidade());
+                    return new ItemPecaOrdemServico(itemPecaOrcamento.getPeca(), itemPecaOrcamento.getQuantidade(), ordemServico);
+                }).toList();
+
+        ordemServico.executar(itemPecaOS, orcamento.getServicos());
+        ordemServicoRepository.save(ordemServico);
+        return ordemServico;
+
+    }
+
+    public void finalizar(Long idOrdemServico) {
+        var ordemServico = buscarOrdemServico(idOrdemServico);
+        ordemServico.finalizar();
+        ordemServicoRepository.save(ordemServico);
+    }
+
+    public OrdemServico entregar(Long idOrdemServico) {
+        var ordemServico = buscarOrdemServico(idOrdemServico);
+        ordemServico.entregar();
+        ordemServicoRepository.save(ordemServico);
+        return ordemServico;
+    }
+
+    private OrdemServico buscarOrdemServico(Long idOrdemServico) {
+        return this.ordemServicoRepository
+                .findById(idOrdemServico)
+                .orElseThrow(RuntimeException::new);
     }
 }
