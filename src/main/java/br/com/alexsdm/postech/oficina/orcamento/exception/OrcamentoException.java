6eb76a4656f4de33e4c214a236326448f5f5d559
Alexssander Fonseca <alex.marques.fa@gmail.com>
2025-08-08 18:05:13 -0300

    feat: demais features

---
+++ src/main/java/br/com/alexsdm/postech/oficina/orcamento/exception/OrcamentoException.java
@@ -0,0 +1,7 @@
+package br.com.alexsdm.postech.oficina.orcamento.exception;
+
+public class OrcamentoException extends RuntimeException {
+    public OrcamentoException(String message) {
+        super(message);
+    }
+}
--- src/main/java/br/com/alexsdm/postech/oficina/peca/service/domain/PecaDomainService.java
+++ src/main/java/br/com/alexsdm/postech/oficina/peca/service/domain/PecaDomainService.java
@@ -1,6 +1,7 @@
 package br.com.alexsdm.postech.oficina.peca.service.domain;
 
 
+import br.com.alexsdm.postech.oficina.peca.exception.QuantidadeIndisponivelException;
 import br.com.alexsdm.postech.oficina.peca.model.Peca;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,7 @@ public class PecaDomainService {
 
     public void retirarItemEstoque(Peca peca, Integer quantidade) {
         if (quantidade > peca.getQuantidadeEstoque()) {
-            throw new RuntimeException();
+            throw new QuantidadeIndisponivelException();
         }
         peca.retirar(quantidade);
     }
