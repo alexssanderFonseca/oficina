1c52c95760c2cd302a880aab8389a714708a2474
Alexssander Fonseca <alex.marques.fa@gmail.com>
2025-10-12 19:12:09 -0300

    feat: build

--- infra/terraform/eks.tf
+++ infra/terraform/eks.tf
@@ -10,6 +10,9 @@ module "eks" {
 
   vpc_id         = module.vpc.vpc_id
   subnet_ids     = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
   create_kms_key = false
 
   cluster_encryption_config = {
