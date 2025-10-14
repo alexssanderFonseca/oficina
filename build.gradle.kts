7f58d8d5ff921fa2b1252f3d3e09f259459ac42e
Alexssander Fonseca <alex.marques.fa@gmail.com>
2025-10-14 14:06:01 -0300

    feat: ajustes

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+    implementation ("org.springframework.boot:spring-boot-starter-actuator")
     implementation("jakarta.inject:jakarta.inject-api:2.0.1")
     implementation("org.mapstruct:mapstruct:1.6.3")
     implementation("org.postgresql:postgresql:42.7.3")
