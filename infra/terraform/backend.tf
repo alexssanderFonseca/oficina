72fb18f403bfb6e7a851485b0107d6d477d0a7c1
Alexssander Fonseca <alex.marques.fa@gmail.com>
2025-12-09 08:23:20 -0300

    feat: ajuste infra eks

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tfstate-fiap-alex"
-    key            = "oficina/terraform.tfstate"
+    key            = "tfstate-fiap-alex-academy"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true
